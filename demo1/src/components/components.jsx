610a410e1db30f9ea923a66003d9da22c0ef2d01
Hamza Khaliq <hamzakhaliq2002@gmail.com>
2023-06-15 13:49:24 -0400

    updated ContactCard class

---
+++ demo1/src/components/components.jsx
@@ -0,0 +1,56 @@
+import React, { Component, ReactPropType} from 'react'
+
+
+class ContactCards extends Component{
+
+    constructor(props){
+        super(props)
+        this.state = {
+            contactName1: "Bruno",
+            phoneNumber1: "555-555-5555",
+            email1: "yadayadayada@apple.com",
+            workNo1: "111-111-1111",
+
+            contactName2: "Jessica",
+            phoneNumber2: "666-666-6666",
+            email2: "nomnomnom@microsoft.com",
+            workNo2: "222-222-2222",
+
+            contactName3: "Alina",
+            phoneNumber3: "777-666-5555",
+            email3: "lololololol@google.com",
+            workNo3: "333-333-3333"
+        }
+
+    }
+
+    render(){
+        return (
+            <div>
+                <h1> Contact1: {this.state.contactName1}</h1>
+                 <h2>email: {this.state.email1}</h2>
+                 <h2>phonenumber: {this.state.phoneNumber1}</h2>
+                 <h2>workNo1: {this.state.workNo1}</h2>
+                 <br />
+
+                 <h1> Contact2: {this.state.contactName2}</h1>
+                 <h2>email2: {this.state.email2}</h2>
+                 <h2>phonenumber2: {this.state.phoneNumber2}</h2>
+                 <h2>workNo2: {this.state.workNo2}</h2>
+                 <br />
+
+                 <h1> Contact3: {this.state.contactName3}</h1>
+                 <h2>email3: {this.state.email3}</h2>
+                 <h2>phonenumber3: {this.state.phoneNumber3}</h2>
+                 <h2>workNo3: {this.state.workNo3}</h2>
+
+
+
+
+            </div>
+        )
+    }
+
+}
+
+export default ContactCards
